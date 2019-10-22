65af24034bceda4fd66836bfd45c9c2dc4c67b1f
EdCarrasco <carrasco.edw@gmail.com>
2019-10-21 20:31:43 -0700

    added components folder and initial FilterPopup.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './App.css';
 
+import FilterPopup from './components/FilterPopup';
+
+class Welcome extends React.Component {
+	render() {
+		return <h1>Welcome, {this.props.name}</h1>;
+	}
+}
+
+
 function App() {
   return (
     <div className="Hello-Text">
-      Hello world
+      Hello world 123 123
+      <Welcome name="Edward" />
+      <FilterPopup name="test" />
     </div>
   );
 }
---
+++ src/components/FilterPopup.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class FilterPopup extends React.Component {
+    render() {
+        return (
+            <div>FilterPopup {this.props.name}</div>
+        );
+    }
+}
+
+export default FilterPopup;
