7dfd8e58fa1d01ffeba788583d583a3896e270d1
bennypham <benny.npham@gmail.com>
2019-10-24 19:48:16 -0700

    Still working and understanding branches

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 
 import FilterPopup from './components/FilterPopup';
+import CarInfo from './components/CarInfo';
 
 class Welcome extends React.Component {
 	render() {
@@ -9,13 +10,13 @@ class Welcome extends React.Component {
 	}
 }
 
-
 function App() {
   return (
     <div className="Hello-Text">
       Hello world 123 123
       <Welcome name="Edward" />
       <FilterPopup name="test" />
+      <CarInfo />
     </div>
   );
 }
