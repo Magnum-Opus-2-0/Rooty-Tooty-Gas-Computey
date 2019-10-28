6f63d8e31a015cfbb6925a99706335a2a714fd3e
RAMcGuyer <ramcguyer@gmail.com>
2019-10-27 18:13:40 -0700

    Need help sorted out geolocated coords

--- src/components/GasStationData.js
+++ src/components/GasStationData.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { geolocated } from "react-geolocated";
 import './styles/GasStationData.css'
 
 /**
@@ -31,6 +32,10 @@ class GasStationContainer extends React.Component {
     handleClick() {
         console.log('FIND Clicked');
         // Todo: Get location
+        if(this.props.isGeolocationAvailable && this.props.isGeolocationEnabled){
+            console.log('Longitude: ' + this.props.coords.longitude);
+            console.log('Latitude: ' + this.props.coords.latitude);
+        }
         // Todo: Access Gas Station API
     }
 
@@ -158,5 +163,11 @@ function FindStations(props) {
         </button>
     );
 }
+export default geolocated({
+    positionOptions: {
+        enableHighAccuracy: false,
+    },
+    userDecisionTimeout: 10000,
+})(GasStationContainer);
 
-export default GasStationContainer
+
