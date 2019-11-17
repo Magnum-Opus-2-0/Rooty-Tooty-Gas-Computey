aad8d4e301e5d530412de9a0b8102def48668b1c
Benny Pham <benny.npham@gmail.com>
2019-11-16 20:20:12 -0800

    Dropdown Menu With Options (#43)
    * * Users can select year of car
    
    * Users can select make of car
    
    * Users can select model of car
    
    * Users can select option of car
    
    * Will return car data based on user input from drop down menu
    
    * * Commented functions for my peers to understand

--- src/App.js
+++ src/App.js
@@ -3,11 +3,9 @@ import './App.css';
 import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import FilterPopup from './components/FilterPopup.js';
-import DropdownMenu from './components/CarInfo.js';
 import GasStationContainer from './components/GasStationData.js';
 import Home from './components/Home.js';
 import Header from './components/Header.js';
-import CarInfo from './components/CarInfo.js';
 import Find from './components/Find.js';
 import CarOptions from './components/CarOptions.js';
 import Layout  from './components/Layout';
--- src/components/CarOptions.js
+++ src/components/CarOptions.js
@@ -1,9 +1,228 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Button, Form, FormGroup, Label, Input, Col } from 'reactstrap';
+import FuelEconomyGov from '../data/FuelEconomyGov';
 
-const CarOptions = () => (
-    <div>
-        <h1>Car options</h1>
-    </div>
-)
+const FuelEconomy = new FuelEconomyGov();
 
-export default CarOptions;
+class DropdownMenu extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentYear: "",
+            make: [],
+            currentMake: "",
+            model: [],
+            currentModel: "",
+            option: [],
+            optionText: [],
+            currentOption: "",
+            id: [],
+            currentID: "",
+            currentCar: null,
+            optionState: null
+        };
+
+        this.yearChange = this.yearChange.bind(this);
+        this.makeChange = this.makeChange.bind(this);
+        this.modelChange = this.modelChange.bind(this);
+        this.optionChange = this.optionChange.bind(this);
+    }
+
+    /**
+    *   Passes in the event which is the year selected
+    *   Saves the year to the state currentYear
+    *   Calls fetchMakesBy which passes in currentYear
+    *   @returns {HTMLElement} A <option> containing car makes
+    */
+    yearChange(event) {
+        // When user selects multiple fields but decides to select another year
+        // This.setState will clear the make, model, and option dropdown menu
+        this.setState({
+            make: <option></option>,
+            model: <option></option>,
+            option: <option></option>
+        });
+        // console.log("Year selected")
+        this.setState({ currentYear: event.target.value }, () => {
+            let make = FuelEconomy.fetchMakesBy(this.state.currentYear);
+            // Adds one or more elements to the beginning of an array and returns the new length of the array
+            make.unshift("")
+            this.setState({
+                make: make.map(make => {
+                    return (
+                        <option>
+                            {make}
+                        </option>
+                    );
+                })
+            })
+            // console.log(this.state.currentYear)
+        });
+    }
+
+    /**
+    *   Passes in the event which is the make selected
+    *   Saves the make to the state currentMake
+    *   Calls fetchModelsBy which passes in currentYear and currentMake
+    *   @returns {HTMLElement} A <option> containing car models
+    */
+
+    makeChange(event) {
+        // When user selects multiple fields but decides to select another make
+        // This.setState will clear model and option dropdown menu
+        this.setState({
+            model: <option></option>,
+            option: <option></option>
+        });
+        // console.log("Make selected")
+        this.setState({ currentMake: event.target.value }, () => {
+            let model = FuelEconomy.fetchModelsBy(this.state.currentYear, this.state.currentMake);
+            // Adds one or more elements to the beginning of an array and returns the new length of the array
+            model.unshift("")
+            this.setState({
+                model: model.map(model => {
+                    return (
+                        <option>
+                            {model}
+                        </option>
+                    );
+                })
+            })
+            // console.log(this.state.currentMake)
+        });
+
+    }
+
+    /**
+    *   Passes in the event which is the model selected
+    *   Saves the model to the state currentModel
+    *   Calls fetchOptionsBy which passes in currentYear, currentMake, currentModel
+    *   Maps the car id to state id [] based on available options
+    *   Ex: Array [ " ", 39557 ]
+    *   Maps the car opt to state optionText [] based on available options
+    *   Ex: Array [ " ", "Auto (AM-S8), 4 cyl, 2.4 L" ]
+    *   @returns {HTMLElement} A <option> containing car options
+    */
+
+    modelChange(event) {
+        // When user selects multiple fields but decides to select another model
+        // This.setState will clear the option dropdown menu
+        this.setState({
+            option: <option></option>
+        });
+        // console.log("Model selected")
+        this.setState({ currentModel: event.target.value }, () => {
+            let option = FuelEconomy.fetchOptionsBy(this.state.currentYear, this.state.currentMake, this.state.currentModel)
+            // Adds one or more elements to the beginning of an array and returns the new length of the array
+            option.unshift("")
+            this.setState({
+                id: option.map(option => {
+                    return option.id ? option.id: " "
+
+                }),
+                optionText: option.map(option => {
+                    return option.opt ? option.opt : " "
+
+                }),
+                option: option.map(option => {
+                    return (
+                        <option>
+                            {option.opt}
+                        </option>
+                    );
+                })
+            }, () => {
+                    // console.log(this.state.id)
+                    // console.log(this.state.optionText)
+            })
+            // console.log(this.state.currentModel)
+        });
+    }
+
+    /**
+    *   Passes in the event which is the option selected
+    *   Saves the option to the state currentOption
+    *   Saves the id to the state currentID
+    *   Calls fetchCarBy which passes in currentID
+    */
+
+    optionChange(event) {
+        console.log("Option selected")
+        this.setState({ currentOption: event.target.value }, () => {
+            this.setState({
+                currentID: this.state.id[this.state.optionText.indexOf(this.state.currentOption)]
+
+            }, () => {
+                    // console.log("Button clicked")
+                    if (this.state.currentID) {
+                        this.setState({
+                            currentCar: FuelEconomy.fetchCarBy(this.state.currentID)
+                        }, () => {
+                            // console.log(this.state.currentCar)
+                        })
+                    }
+            })
+
+        });
+    }
+    /**
+     *  Renders the list of car years
+     *
+     *  @returns {HTMLElement} An <option> containing car years
+    */
+    render() {
+        let years = FuelEconomy.fetchYears();
+        // Adds one or more elements to the beginning of an array and returns the new length of the array
+        years.unshift("");
+        years = years.map(year => {
+            return (
+                <option>
+                    {year}
+                </option>
+            );
+        })
+
+        return (
+            <Form onSubmit={this.handleSubmit}>
+                <FormGroup row>
+                    <Label for="exampleSelect" sm={2}>Select year</Label>
+                    <Col sm={10}>
+                        <Input type="select" name="select" id="exampleSelect" onChange={this.yearChange}>
+                            {years}
+                        </Input>
+                    </Col>
+                </FormGroup>
+                <FormGroup row>
+                    <Label for="exampleSelect" sm={2}>Select make</Label>
+                    <Col sm={10}>
+                        <Input type="select" name="select" id="exampleSelect" onChange={this.makeChange}>
+                            {this.state.make}
+                        </Input>
+                    </Col>
+                </FormGroup>
+                <FormGroup row>
+                    <Label for="exampleSelect" sm={2}>Select model</Label>
+                    <Col sm={10}>
+                        <Input type="select" name="select" id="exampleSelect" onChange={this.modelChange}>
+                            {this.state.model}
+                        </Input>
+                    </Col>
+                </FormGroup>
+                <FormGroup row>
+                    <Label for="exampleSelect" sm={2}>Select options</Label>
+                    <Col sm={10}>
+                        <Input type="select" name="select" id="exampleSelect" onChange={this.optionChange}>
+                            {this.state.option}
+                        </Input>
+                    </Col>
+                </FormGroup>
+                {/* Todo: Make submit button do something  */}
+                <Button>
+                    Submit
+                </Button>
+            </Form>
+        );
+    }
+}
+
+export default DropdownMenu;
