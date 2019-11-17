5e89f0708081082ecf8020842345af431c14ba0c
EdCarrasco <carrasco.edw@gmail.com>
2019-11-17 00:10:30 -0800

    added awesome dropdown menu for filter and buttons

--- src/components/GasStationFilterContainer.js
+++ src/components/GasStationFilterContainer.js
@@ -3,17 +3,47 @@ import FilterPopup from './FilterPopup.js';
 import GasStationContainer from './GasStationData';
 import {Nav, Navbar, NavItem, Collapse} from 'reactstrap';
 import {Dropdown, DropdownToggle, DropdownMenu, DropdownItem} from 'reactstrap';
+import {Button, ButtonGroup} from 'reactstrap';
 
 export default class GasStationFilterContainer extends React.Component {
 	constructor(props) {
 		super(props)
 		this.state = {
         	selectedFilters: [],
-        	filterDropdownOpen: false
+        	filterDropdownOpen: false,
+        	availableFilters: ['Arco', 'Chevron', 'Texaco', 'Mobil', '76'],
+        	filterButtons: []
         }
 
         this.setSelectedFilters = this.setSelectedFilters.bind(this)
         this.filterDropdownToggle = this.filterDropdownToggle.bind(this)
+        //this.addFilter = this.addFilter.bind(this)
+
+
+        this.filterOptions = this.getFilterOptions(this.state.availableFilters)
+        this.filterButtons = []
+    }
+
+    test() {
+    	console.log("TEST TEST TEST")
+    }
+
+    getFilterOptions(filterList) {
+    	const optionStyle = {
+    		'background-color': 'white',
+    		'color': 'black',
+    		'border': 'none',
+    		'width': '100%',
+    	}
+    	let elemList = filterList.map(filter => {
+    		console.log(this)
+			return (
+				<React.Fragment>
+					<Button style={optionStyle} onClick={(event) => this.addFilter(filter)}>{filter}</Button><br/>
+				</React.Fragment>
+			)
+		})
+		return elemList
     }
 
     filterDropdownToggle() {
@@ -30,29 +60,67 @@ export default class GasStationFilterContainer extends React.Component {
         this.setState({selectedFilters : list})
     }
 
+    setFilterButtons(list) {
+    	const buttonStyle = {'margin-right': '0.1em'}
+    	let filterButtons = list.map(filter => {
+			return (
+				<React.Fragment>
+					<Button style={buttonStyle} onClick={(event) => this.removeFilter(filter)}>{filter}</Button>
+				</React.Fragment>
+			)
+		})
+    	this.setState({filterButtons: filterButtons})
+    }
+
+    addFilter(value) {
+    	if (this.state.selectedFilters.includes(value)) {
+    		return;
+    	}
+
+    	const list = this.state.selectedFilters.slice()
+    	list.push(value)
+    	this.setState({selectedFilters: list})
+
+    	this.setFilterButtons(list)
+    }
+
+    removeFilter(filter) {
+		let list = this.state.selectedFilters.slice()
+    	const index = list.indexOf(filter)
+    	if (index >= 0) {
+    		list.splice(index, 1)
+    		this.setState({selectedFilters: list})
+    	}
+
+    	this.setFilterButtons(list)
+    }
+
+    test2() {
+    	console.log("test2")
+    }
+
 	render() {
+		const buttonGroupStyle = {'margin-left':'0.4em'}
+
 		return (
 			<React.Fragment>
 				{/* Navigation bar for the filters */}
 				<Navbar color="light" light expand="md">
 					<Collapse isOpen={true} navbar>
-						<Dropdown isOpen={this.state.filterDropdownOpen} toggle={this.filterDropdownToggle} >
-							<DropdownToggle caret>
-								Filters
-							</DropdownToggle>
-							{/*<DropdownMenu>
-								<DropdownItem header>Gas Stations</DropdownItem>
-								<DropdownItem>Item A</DropdownItem>
-								<DropdownItem>Item B</DropdownItem>
-							</DropdownMenu>
-						</Dropdown>*/}
-						
-						{/*<Nav>
-							<NavItem>Test1</NavItem>
-							<NavItem>Test2</NavItem>
-							<NavItem>Test3</NavItem>
-
-						</Nav>*/}
+						<Nav>
+							<Dropdown isOpen={this.state.filterDropdownOpen} toggle={this.filterDropdownToggle} >
+								<DropdownToggle caret>
+									Filters
+								</DropdownToggle>
+								<DropdownMenu>
+									{this.filterOptions}
+								</DropdownMenu>
+							</Dropdown>
+							<div style={buttonGroupStyle}>
+								{this.state.filterButtons}
+							</div>
+							<DropdownItem onClick={this.test}>Click me!</DropdownItem>
+						</Nav>
 					</Collapse>
 				</Navbar>
 
