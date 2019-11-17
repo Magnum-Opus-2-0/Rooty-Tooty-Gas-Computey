c35c6a92d8ca60e6ccb5edbb5577196103a4e195
bennypham <benny.npham@gmail.com>
2019-11-16 20:10:58 -0800

    * Commented functions for my peers to understand

--- src/components/CarOptions.js
+++ src/components/CarOptions.js
@@ -28,7 +28,15 @@ class DropdownMenu extends Component {
         this.optionChange = this.optionChange.bind(this);
     }
 
+    /**
+    *   Passes in the event which is the year selected
+    *   Saves the year to the state currentYear
+    *   Calls fetchMakesBy which passes in currentYear
+    *   @returns {HTMLElement} A <option> containing car makes
+    */
     yearChange(event) {
+        // When user selects multiple fields but decides to select another year
+        // This.setState will clear the make, model, and option dropdown menu
         this.setState({
             make: <option></option>,
             model: <option></option>,
@@ -37,6 +45,7 @@ class DropdownMenu extends Component {
         // console.log("Year selected")
         this.setState({ currentYear: event.target.value }, () => {
             let make = FuelEconomy.fetchMakesBy(this.state.currentYear);
+            // Adds one or more elements to the beginning of an array and returns the new length of the array
             make.unshift("")
             this.setState({
                 make: make.map(make => {
@@ -51,7 +60,16 @@ class DropdownMenu extends Component {
         });
     }
 
+    /**
+    *   Passes in the event which is the make selected
+    *   Saves the make to the state currentMake
+    *   Calls fetchModelsBy which passes in currentYear and currentMake
+    *   @returns {HTMLElement} A <option> containing car models
+    */
+
     makeChange(event) {
+        // When user selects multiple fields but decides to select another make
+        // This.setState will clear model and option dropdown menu
         this.setState({
             model: <option></option>,
             option: <option></option>
@@ -59,6 +77,7 @@ class DropdownMenu extends Component {
         // console.log("Make selected")
         this.setState({ currentMake: event.target.value }, () => {
             let model = FuelEconomy.fetchModelsBy(this.state.currentYear, this.state.currentMake);
+            // Adds one or more elements to the beginning of an array and returns the new length of the array
             model.unshift("")
             this.setState({
                 model: model.map(model => {
@@ -74,13 +93,27 @@ class DropdownMenu extends Component {
 
     }
 
+    /**
+    *   Passes in the event which is the model selected
+    *   Saves the model to the state currentModel
+    *   Calls fetchOptionsBy which passes in currentYear, currentMake, currentModel
+    *   Maps the car id to state id [] based on available options
+    *   Ex: Array [ " ", 39557 ]
+    *   Maps the car opt to state optionText [] based on available options
+    *   Ex: Array [ " ", "Auto (AM-S8), 4 cyl, 2.4 L" ]
+    *   @returns {HTMLElement} A <option> containing car options
+    */
+
     modelChange(event) {
+        // When user selects multiple fields but decides to select another model
+        // This.setState will clear the option dropdown menu
         this.setState({
             option: <option></option>
         });
         // console.log("Model selected")
         this.setState({ currentModel: event.target.value }, () => {
             let option = FuelEconomy.fetchOptionsBy(this.state.currentYear, this.state.currentMake, this.state.currentModel)
+            // Adds one or more elements to the beginning of an array and returns the new length of the array
             option.unshift("")
             this.setState({
                 id: option.map(option => {
@@ -100,16 +133,23 @@ class DropdownMenu extends Component {
                 })
             }, () => {
                     // console.log(this.state.id)
+                    // console.log(this.state.optionText)
             })
             // console.log(this.state.currentModel)
         });
     }
 
+    /**
+    *   Passes in the event which is the option selected
+    *   Saves the option to the state currentOption
+    *   Saves the id to the state currentID
+    *   Calls fetchCarBy which passes in currentID
+    */
+
     optionChange(event) {
-        // console.log("Option selected")
+        console.log("Option selected")
         this.setState({ currentOption: event.target.value }, () => {
             this.setState({
-
                 currentID: this.state.id[this.state.optionText.indexOf(this.state.currentOption)]
 
             }, () => {
@@ -125,10 +165,14 @@ class DropdownMenu extends Component {
 
         });
     }
-
+    /**
+     *  Renders the list of car years
+     *
+     *  @returns {HTMLElement} An <option> containing car years
+    */
     render() {
-
         let years = FuelEconomy.fetchYears();
+        // Adds one or more elements to the beginning of an array and returns the new length of the array
         years.unshift("");
         years = years.map(year => {
             return (
@@ -172,6 +216,10 @@ class DropdownMenu extends Component {
                         </Input>
                     </Col>
                 </FormGroup>
+                {/* Todo: Make submit button do something  */}
+                <Button>
+                    Submit
+                </Button>
             </Form>
         );
     }
