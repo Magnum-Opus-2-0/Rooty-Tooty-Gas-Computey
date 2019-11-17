4ccfa4182f341f39990c84fc427b908171a99784
Michael Bradley <mbrad94@gmail.com>
2019-11-16 22:43:04 -0800

    Add UserData convenience methods
    Write associated tests.

--- src/data/StationCalculation.js
+++ src/data/StationCalculation.js
@@ -1,3 +1,4 @@
+import user from './UserData'
 
 class StationCalculation {
 
@@ -108,6 +109,24 @@ class StationCalculation {
     }
 
     /**
+     * Convenience method to compare cost using UserData.
+     *
+     * This method can be called directly by Array.sort.
+     *
+     * @param {Object}  stationA        An object with a coords property to
+     *                                  compare.
+     * @param {Object}  stationB        An object with a coords property to
+     *                                  compare.
+     * @returns {number}    -1 if stationA has a lower cost than stationB, 0 if
+     *                      stationA has an equivalent cost to stationB, 1 if
+     *                      stationB has a lower cost than stationA, or null if
+     *                      there was an invalid calculation.
+     */
+    compareCostUser(stationA, stationB) {
+        return this.compareCost(stationA, stationB, user.mpg, user.tankSize, user.tankFill, user.location);
+    }
+
+    /**
      * Calculate the efficiency of driving to the specified gas station.
      *
      * Function assumes the user will fill their tank when they arrive at the
@@ -151,6 +170,20 @@ class StationCalculation {
 
         let dist = this.calcDistance(station.coords, userLocation);
         return station.price * ((dist / mpg) + volumeMax * (1 - volumeCur));
+    }
+
+    /**
+     * Convenience function to calculate cost using UserData.
+     *
+     * @param {object}  station         An object representing the gas station.
+     *                                  The object must contain a coords
+     *                                  property with latitude and longitude
+     *                                  properties and a price property.
+     * @returns {number}    A number representing the efficiency of driving to
+     *                      the specified gas station.
+     */
+    calcCostUser(station) {
+        return this.calcCost(user.mpg, user.tankSize, user.tankFill, station, user.location);
     }
 
     /**
