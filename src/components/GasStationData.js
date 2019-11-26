a1c46c1632f57c852bee7e0a6572445caee8f94e
Michael Bradley <mbrad94@gmail.com>
2019-11-25 18:45:02 -0800

    Fix number of stations displayed after filtering
    Previously, we filtered the already sorted and sliced data, so it would
    display <= 5 stations. Now we always display 5 stations regardless of
    what filters are selected.

--- src/components/GasStationData.js
+++ src/components/GasStationData.js
@@ -144,6 +144,7 @@ class GasStationContainer extends React.Component {
         let allStationsRef = (this.props.firebase) ? this.props.firebase.getAllStationsRef() : null;
 
         function onData(data) {
+            console.log('ON DATA CALLED');
 
             let allStationsRaw = data.val();
             let stationNames = new Set();
@@ -178,28 +179,12 @@ class GasStationContainer extends React.Component {
             // Here, export stationNames to GasStationFilterContainer
             this.props.retrieveStationNames(Array.from(stationNames));
 
-            //Todo: filter and sort array here
-            let sc = new StationCalculation();  // TODO replace with compareCost once we have the info
 
-            // Sort by price * distance
-            allStationsArr.sort((stationA, stationB) => {
-
-                // If the user inputted car options let's use the smart calculation.
-                // Otherwise we'll use pure distance.
-                // TODO: Put a switch statement here to let the user decide the type of calculation
-                if (user.tankFill != -1 && user.mpg != -1 && user.tankSize != -1) {
-                    return sc.compareCostUser(stationA, stationB, user);
-                }
-
-                return sc.compareDistance(stationA, stationB, user.location);
-            });
-
-            let fiveStations = allStationsArr.slice(0,5);
-
-            this.setState({ stationsData: fiveStations});
+            this.setState({ stationsData: allStationsArr.slice()});
             this.setState({ findClicked: true});
 
-        };
+        }
+
         onData = onData.bind(this);
         if (allStationsRef) {
             allStationsRef.on("value", onData);
@@ -221,6 +206,27 @@ class GasStationContainer extends React.Component {
         }
 
         return true;
+    }
+
+    sortData(stationsData) {
+        //Todo: filter and sort array here
+        let sc = new StationCalculation();  // TODO replace with compareCost once we have the info
+
+        // Sort by price * distance
+        let allStationsArr = stationsData.slice();
+        allStationsArr.sort((stationA, stationB) => {
+
+            // If the user inputted car options let's use the smart calculation.
+            // Otherwise we'll use pure distance.
+            // TODO: Put a switch statement here to let the user decide the type of calculation
+            if (user.tankFill != -1 && user.mpg != -1 && user.tankSize != -1) {
+                return sc.compareCostUser(stationA, stationB, user);
+            }
+
+            return sc.compareDistance(stationA, stationB, user.location);
+        });
+
+        return allStationsArr.slice(0,5);
     }
 
     /**
@@ -264,8 +270,9 @@ class GasStationContainer extends React.Component {
      *                          related components.
      */
     render() {
-        let filteredData = this.filterByGasStationName(this.state.stationsData, this.props.selectedFilters);
-        filteredData = this.filterByDistance(filteredData, this.props.maxDistance);
+        let filteredData = this.filterByDistance(this.state.stationsData, this.props.maxDistance);
+        filteredData = this.filterByGasStationName(filteredData, this.props.selectedFilters);
+        filteredData = this.sortData(filteredData);
         let mapStyle = {'height': '80vh', 'width': '90%'};
 
         return(
